782c1ecbb7b0f2fb3063f731c478229552ec6a56
Aviax <87578989+TeamAviax@users.noreply.github.com>
2023-09-01 00:22:19 +0530

    Update heroku.py

--- AviaxMusic/plugins/sudo/heroku.py
+++ AviaxMusic/plugins/sudo/heroku.py
@@ -21,7 +21,7 @@ from AviaxMusic.utils.database import (get_active_chats,
                                        remove_active_chat,
                                        remove_active_video_chat)
 from AviaxMusic.utils.decorators.language import language
-from AviaxMusic.utils.pastebin import Yukkibin
+from AviaxMusic.utils.pastebin import Aviaxbin
 
 # Commands
 GETLOG_COMMAND = get_command("GETLOG_COMMAND")
@@ -47,7 +47,7 @@ async def log_(client, message, _):
             if HAPP is None:
                 return await message.reply_text(_["heroku_1"])
             data = HAPP.get_log()
-            link = await Yukkibin(data)
+            link = await Aviaxbin(data)
             return await message.reply_text(link)
         else:
             if os.path.exists(config.LOG_FILE_NAME):
@@ -60,7 +60,7 @@ async def log_(client, message, _):
                     NUMB = 100
                 for x in lines[-NUMB:]:
                     data += x
-                link = await Yukkibin(data)
+                link = await Aviaxbin(data)
                 return await message.reply_text(link)
             else:
                 return await message.reply_text(_["heroku_2"])
